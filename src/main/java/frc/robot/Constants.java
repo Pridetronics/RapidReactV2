02ebba818b7b99e730b2ca83e5dedf22d61362b8
Katerina Deeter <46850117+KaterinaDeeter@users.noreply.github.com>
2022-02-23 10:25:44 -0700

    Added servo and reorganized RobotContainer+Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,21 +13,26 @@ package frc.robot;
  */
 public final class Constants // Note for self: Define integers here. Button numbers and the addresses of the motors.
 {
+    //Joysticks and Button Numbers--
     public static final int kJoystickDriverID = 0; // For Joystick 0
     public static final int kJoystickShooterID = 1; // For Joystick 1
-    public static final int shooterButtonNumber = 4; // All values will need to be adjusted for true values. 
+
+    public static final int shooterButtonNumber = 4; // All values will need to be adjusted for true values. SOLVE CONFLICTS
     public static final int intakeButtonNumber = 2;
     public static final int visionModeButtonNumber = 3;
-    public static final int kFrontLeftID = 2; // For all of the drive motors
-    public static final int kRearLeftID = 4;
-    public static final int kFrontRightID = 5; //Switch with shooter motor after testing
-    public static final int kRearRightID = 3;
+    public static final int climbButtonNumber = 3; //For climb Button on the Gamepad
+    public static final int cancellationButton1 = 1; //For climb's cancellation button on the Gamepad
+    public static final int cancellationButton2 = 2; //For climb's cancellation button on the Gamepad
+    public static final int addButtonNumber = 5; //For climb's adding one button on the Gamepad
 
-     public static final int climbButtonNumber = 3; //For climb Button on the Gamepad
-     public static final int cancellationButton1 = 1; //For climb's cancellation button on the Gamepad
-     public static final int cancellationButton2 = 2; //For climb's cancellation button on the Gamepad
-     public static final int addButtonNumber = 5; //For climb's adding one button on the Gamepad
-    public static final int kShooterID = 1; // For the Shooter Motor
+    //Drive--
+    public static final int kFrontLeftCANID = 2; // For all of the drive motors
+    public static final int kRearLeftCANID = 4;
+    public static final int kFrontRightCANID = 1; //Switch with shooter motor after testing
+    public static final int kRearRightCANID = 3;
+
+    //Shooter--
+    public static final int kShooterCANID = 5; // For the Shooter Motor
     public static final int shooterDefaultSpeed = 3000; //This will act as a minimum speed if no target is found. Allows the Gate to release. 
     public static final double SHOOTER_kP = 0.0002;
     public static final double SHOOTER_kI = 0.000001;
@@ -35,20 +40,10 @@ public final class Constants // Note for self: Define integers here. Button numb
     public static final double MAX_NEO_RPM = 5676.0;
     public static final double SHOOTER_MAX_OUTPUT = 6000.0;
     public static final double SHOOTER_MIN_OUTPUT = -6000.0;
-    public static final int kShooterGateForwardID = 4; // For use in pneumatics system (same with the one below)
-    public static final int kShooterGateReleaseID = 5;
-     public static final int kClimbChannel = 7; //For Climb Motor
-     public static final int kPistonFirstClimbChannel = 6; //For piston
-     public static final int kPistonFirstReverseClimbChannel = 7; //For piston
-     public static final double ClimbMotorSpeed = 1.0; //Sets speed for climb motor
-     public static final double InverseClimbMotorSpeed = -1.0; //Sets speed for climb motor
-     public static final int upperClimbLimitSwitchChannel = 1; //Upper Limit Switch Channel
-     public static final int lowerClimbLimitSwitchChannel = 0; //Lower Limit Switch Channel
-     public static final int kEncoderCountsPerRev = 42; //Ask about the encoder
-     private static final double kArmDiameter = 2.38; //Arms' diameter in inches
-     public static final double kEncoderPositionConversionFactor = kArmDiameter * Math.PI; //Sets encoder to read position in inches
-
-    //Shooter RPM Values-- I'm a terrible person. I will try to make a list later but I'm stupid so bad naming conventions.
+    public static final int kShooterServoPWMID = 0; 
+    // public static final int kShooterGateForwardID = 4; //Swap out for Servo ID-- leaving this just in case (REMOVE LATER)
+    // public static final int kShooterGateReleaseID = 5;
+    //RPM Values for Shooter-- Needs to be adjusted later.  (HOPEFULLY I CAN GET RID OF THIS... Ask me how later)
     public static final int shooterRPM17 = 5000;
     public static final int shooterRPM16 = 4750;
     public static final int shooterRPM15 = 4500;
@@ -57,5 +52,19 @@ public final class Constants // Note for self: Define integers here. Button numb
     public static final int shooterRPM12 = 3750;
     public static final int shooterRPM11 = 3500;
     public static final int shooterRPM10 = 3250;
-    public static final int kIntakeID = 6;
+
+    //Climb--
+    public static final int kClimbCANID = 7; //For Climb Motor
+    public static final int kPistonFirstClimbChannel = 6; //For piston
+    public static final int kPistonFirstReverseClimbChannel = 7; //For piston
+    public static final double ClimbMotorSpeed = 1.0; //Sets speed for climb motor
+    public static final double InverseClimbMotorSpeed = -1.0; //Sets speed for climb motor
+    public static final int upperClimbLimitSwitchChannel = 1; //Upper Limit Switch Channel
+    public static final int lowerClimbLimitSwitchChannel = 0; //Lower Limit Switch Channel
+    public static final int kEncoderCountsPerRev = 42; //Ask about the encoder-- REACH OUT TO HELEN(THIS ISN'T NEEDED BECAUSE IT HAS DEFAULT)
+    private static final double kArmDiameter = 2.38; //Arms' diameter in inches
+    public static final double kEncoderPositionConversionFactor = kArmDiameter * Math.PI; //Sets encoder to read position in inches
+
+    //Intake--
+    public static final int kIntakeCANID = 6;
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,17 +10,20 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
+import frc.robot.subsystems.Climb;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Intake;
+
 import frc.robot.commands.Autonomous;
 import frc.robot.commands.DriveJoystick;
 import frc.robot.commands.ReleaseGate;
 import frc.robot.commands.ShooterRun;
 import frc.robot.commands.VisionMode;
+import frc.robot.commands.LimelightDistanceFinder;
 import frc.robot.commands.ExtendRetractIntake;
 import frc.robot.commands.IntakeRun;
-import frc.robot.commands.LimelightDistanceFinder;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.AddOne;
-import frc.robot.commands.Autonomous;
 import frc.robot.commands.CancellationButtonsClimb;
 import frc.robot.commands.CancelClimb;
 import frc.robot.commands.ClimbButtonSequence;
@@ -28,33 +31,20 @@ import frc.robot.commands.ClimbPiston;
 import frc.robot.commands.DescendPivotArms;
 import frc.robot.commands.RaisePivotArms;
 
-import frc.robot.subsystems.Climb;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
-import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxRelativeEncoder;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
-
-import frc.robot.commands.Autonomous;
-import frc.robot.commands.ReleaseGate;
-import frc.robot.commands.ShooterRun;
-import frc.robot.commands.ExtendRetractIntake;
-import frc.robot.commands.IntakeRun;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Intake;
-
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -67,21 +57,25 @@ import frc.robot.subsystems.Intake;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-
+  //Drive--
   public static CANSparkMax frontLeft; //Creates all four drive motors
   public static CANSparkMax rearLeft;
   public static CANSparkMax frontRight;
   public static CANSparkMax rearRight;
 
+  //Shooter--
   public static CANSparkMax shooterMotor; // Creates Motor for the shooter
   public static RelativeEncoder shooterEncoder;
   public static SparkMaxPIDController shooterMotorPID;
-  public static DoubleSolenoid shooterBallRelease; // Creates Double Solenoid for the shooter (relates to pistons)
+  // public static DoubleSolenoid shooterBallRelease; // Creates Double Solenoid for the shooter (relates to pistons)
+  public static Servo shooterServo;
 
+  //Intake--
   public static Compressor intakeCompressor;
   public static Solenoid intakePiston;
   public static VictorSP intakeMotor;
 
+  //Climb--
   public static CANSparkMax climbMotor; //Climb Motor
   public static DoubleSolenoid m_climbPiston; //Climb Piston
   public static RelativeEncoder ClimbEncoder;
@@ -89,6 +83,7 @@ public class RobotContainer {
   public static DigitalInput lowerClimbLimitSwitch;
   public static int climbValue; //For climb's add value
 
+  //Climb Commands--
   public static Command ClimbButtonSequence;
   public Command climbSequence;
   public static Command CancelClimb;
@@ -96,14 +91,14 @@ public class RobotContainer {
   public static Command AddOne;
   public Command addOne;
 
-
-  public static Climb climb; //Creates the subsystem for climb
-
+  //Subsystems--
   public static Intake m_intake;
   public static Shooter m_shooter; // Creates the subsytem for shooter
   public static Drive m_drive;
+  public static Climb climb; //Creates the subsystem for climb
   public static Autonomous m_auto;
 
+  //Buttons and Joysticks--
   public JoystickButton shooterButton; // Button for the shooter
   public JoystickButton intakeButton;
   public static JoystickButton climbButton;
@@ -124,16 +119,16 @@ public class RobotContainer {
     joystickShooter = new Joystick(Constants.kJoystickShooterID); // Sets shooter joystick to port 1
 
     // Drive Relevant---
-    frontLeft = new CANSparkMax(Constants.kFrontLeftID, MotorType.kBrushless);
-    rearLeft = new CANSparkMax(Constants.kRearLeftID, MotorType.kBrushless);
-    frontRight = new CANSparkMax(Constants.kFrontRightID, MotorType.kBrushless);
+    frontLeft = new CANSparkMax(Constants.kFrontLeftCANID, MotorType.kBrushless);
+    rearLeft = new CANSparkMax(Constants.kRearLeftCANID, MotorType.kBrushless);
+    frontRight = new CANSparkMax(Constants.kFrontRightCANID, MotorType.kBrushless);
     frontRight.setInverted(true);
-    rearRight = new CANSparkMax(Constants.kRearRightID, MotorType.kBrushless);
+    rearRight = new CANSparkMax(Constants.kRearRightCANID, MotorType.kBrushless);
     rearRight.setInverted(true);
     m_drive = new Drive(joystickDriver);
 
     // Shooter Relevant---
-    shooterMotor = new CANSparkMax(Constants.kShooterID, MotorType.kBrushless);
+    shooterMotor = new CANSparkMax(Constants.kShooterCANID, MotorType.kBrushless);
     shooterMotor.setInverted(true);
     shooterEncoder = shooterMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
  
@@ -141,27 +136,30 @@ public class RobotContainer {
     shooterMotorPID.setP(Constants.SHOOTER_kP);
     shooterMotorPID.setI(Constants.SHOOTER_kI);
     shooterMotorPID.setD(Constants.SHOOTER_kD);
+
     shooterMotorPID.setOutputRange(Constants.SHOOTER_MIN_OUTPUT, Constants.SHOOTER_MAX_OUTPUT);
-    shooterBallRelease = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.kShooterGateForwardID, Constants.kShooterGateReleaseID);
+    // shooterBallRelease = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.kShooterGateForwardID, Constants.kShooterGateReleaseID);
+    shooterServo = new Servo(Constants.kShooterServoPWMID);
     m_shooter = new Shooter(); // Defines the subsystem
     
     // Intake Relevant---
     intakeCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
     intakePiston = new Solenoid(0, PneumaticsModuleType.CTREPCM, 0);
-    intakeMotor = new VictorSP(Constants.kIntakeID);
+    intakeMotor = new VictorSP(Constants.kIntakeCANID);
     m_intake = new Intake();
+
+    //SmartDashboard Relevant-- Remove these during competition time
     SmartDashboard.putData("Autonomous", new Autonomous(m_drive));
     SmartDashboard.putData("Climb Run", new ClimbButtonSequence(climb)); //Puts data on Shuffleboard to use the command
     SmartDashboard.putData("Climb's Sequence", new AddOne(climb)); //Puts data on Shuffleboard to see what stage climbValue is at.
-
     SmartDashboard.putData("Shooter Run", new ShooterRun(m_shooter)); // Puts data on Shuffleboard to use the command.
     SmartDashboard.putData("Release Gate", new ReleaseGate(m_shooter)); // Displays on the screen and can be run by pushing the square. Pretty neat
     SmartDashboard.putData("Intake Run", new IntakeRun(m_intake));
     SmartDashboard.putData("Extend/Retract Intake", new ExtendRetractIntake(m_intake));
     SmartDashboard.putData("Find Distance", new LimelightDistanceFinder(m_shooter));
     SmartDashboard.putData("Change Vision Modes", new VisionMode(m_shooter));    
+    
     configureButtonBindings();
-
     m_drive.setDefaultCommand(new DriveJoystick(m_drive));
   }
 
@@ -182,7 +180,7 @@ public class RobotContainer {
         new WaitCommand(3),
         new IntakeRun(m_intake)));
 
-      //Climb Button Configured
+    //Climb Button Configured
     climbButton = new JoystickButton(joystickShooter, Constants.climbButtonNumber);
     climbSequence = new ClimbButtonSequence(climb);
     climbButton.whenPressed(climbSequence);
@@ -194,10 +192,11 @@ public class RobotContainer {
     CancellationButtonsClimb cancellationButtons = new CancellationButtonsClimb(cancellationButton1, cancellationButton2);
     cancelClimb = new CancelClimb(climb);
     cancellationButtons.whenPressed(cancelClimb);
+    //Need to add encoders, when it is at the bottom you have to make sure the encoders is at 0.
+    //There is also no need to do anything for the stationary arm
+    //Winches is just the one motor going forward and reverse.
   }
-        //Need to add encoders, when it is at the bottom you have to make sure the encoders is at 0.
-        //There is also no need to do anything for the stationary arm
-        //Winches is just the one motor going forward and reverse.
+
   public Command getAutonomousCommand() 
   {
     return m_auto;
