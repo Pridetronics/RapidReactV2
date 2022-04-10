5b1a4ff1914f4c21dec6c51c665e1f16fa5ff0c3
Katerina Deeter <46850117+KaterinaDeeter@users.noreply.github.com>
2022-04-09 21:53:23 -0700

    Commented Vision.java

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,25 +2,32 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+* This is the Vision Subsystem. At this point it is used for Limelight and 
+* functions that interact with it. Because of this, hardware for shooter
+* and drive are also created and used within this file. 
+*/
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import frc.robot.Constants;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.controller.PIDController;
+//Network tables are vital for use of Limelight. 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.ControlType;
-
 import edu.wpi.first.wpilibj.Servo;
 
 public class Vision extends SubsystemBase {
+  //Hardware Relevant
   private CANSparkMax m_frontLeftMotor;
   private CANSparkMax m_rearLeftMotor;
   private CANSparkMax m_frontRightMotor;
@@ -36,6 +43,7 @@ public class Vision extends SubsystemBase {
   private SparkMaxPIDController m_shooterPIDController;
   private Servo m_shooterServo;
 
+  //The following is all involved in Limelight values. Explained further within functions
   private double ty;
   private double tx;
   private double tv;
@@ -72,6 +80,7 @@ public class Vision extends SubsystemBase {
 
   @Override
   public void periodic() {
+    // While there are comments, this is a tricky topic-- check out the documentation if you need help
     NetworkTableInstance inst = NetworkTableInstance.getDefault(); //get a reference to the subtable called "datatable"
     NetworkTable table = inst.getTable("limelight");
     inst.startClientTeam(3853); // Make sure you set this to your team number
@@ -81,8 +90,8 @@ public class Vision extends SubsystemBase {
     NetworkTableEntry aEntry = table.getEntry("ta");
     NetworkTableEntry vEntry = table.getEntry("tv");
 
-    ty = yEntry.getDouble(0.0); // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
     ta = aEntry.getDouble(0.0); // Target Area (0% of image to 100% of image)
+    ty = yEntry.getDouble(0.0); // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
     tx = xEntry.getDouble(0.0); //Horizontal Offset From Crosshair to Target (-27.5 degrees to 27.5 degrees)
     tv = vEntry.getDouble(0.0); // Whether the limelight has any valid targets (0 or 1)
 
@@ -93,15 +102,20 @@ public class Vision extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
+  //Turns out LEDs, was created to be called in Robot Init (so the light wouldn't be on until needed)
   public void lightsOut()
   {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
   }
 
-  public void processingMode() //For the toggle between vision modes. 
+  /*
+  * Processing Mode and Camera mode are for toggling between vision modes. 
+  * Processing mode prepares it for use with shooter/drive. CameraMode is solely for user vision
+  */
+  public void processingMode() 
   {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0); //Sets camMode to processing
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(2); //Tells LEDs to turn on
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(2); //Tells LEDs to turn on (also there is an alternate 0, follows pipeline settings)
   }
 
   public void cameraMode()
@@ -110,6 +124,10 @@ public class Vision extends SubsystemBase {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1); //Forces LEDs off. 
   }
 
+  /* Seek Target: Checks if the Limelight has a target, if it does not, it will utilize PID control to
+  *  adjust until it finds a target. PID control is for movement speed. This is meant to keep it from
+  *  moving too fast (keeping it at 100 RPM). Then sets to 0 once target is found. 
+  */
   public void SeekTarget() 
   {
     if (tv == 0) 
@@ -128,6 +146,10 @@ public class Vision extends SubsystemBase {
     }
   }
 
+  /* TargetAdjust: Meant to center the robot on the horizontal axis, uses the drive train to turn slightly
+  *  based on where it falls within the angles of vision. If the target is too far right, it will turn left.
+  *  Too far left, it will turn right. If it is centered, do nothing. 
+  */
   public void TargetAdjust() {
     if (tx > 3 && tx < 28) // turning left
     {
@@ -152,17 +174,18 @@ public class Vision extends SubsystemBase {
     }
   }
 
+  //findDistance: Calculates distance and converts it into rounded distance for use within the automatic shooter code
   public void findDistance()
   {
-    //Put the full equation for reference of breakdown below
-    heightTotal = 104 - 25.75; //Measures in meters. Change these to the official field values later-- This is for testing INPUT IN INCHES *****
-    angleTotal = 0.628 + ty; //MEASURES IN RADIANS NOT DEGREES. DO NOT MAKE THE SAME MISTAKES (PUT THE VALUES IN AS RADIANS)
+    //Equation for distance is d = (h1 - h2)/(tan(a1 + a2)) //Defaults to meters/radian
+    heightTotal = 104 - 25.75; //Input inches
+    angleTotal = 0.628 + ty; //Input radians
     angleTan = Math.tan(angleTotal);
 
-    initialDistance = heightTotal/angleTan; //Outputs in meters/radian. Returns negative, so there's a negative on the next line
+    initialDistance = heightTotal/angleTan; //Returns negative, so there's a negative on the next line
     distanceInInches =- initialDistance * 13.6; //Converts distance into inches. 
     distanceInFeet = distanceInInches/12; //Converts distance in inches to feet
-    roundedDistance = Math.round(distanceInFeet); //Rounds distance in feet-- I'm hoping to make this process a little smoother but for now this will do.
+    roundedDistance = Math.round(distanceInFeet); //Rounds distance in feet
     
     SmartDashboard.putNumber("Initial Distance", initialDistance); //Puts all these values on the smartdashboard when run. This is solely for testing purposes
     SmartDashboard.putNumber("Distance in Inches", distanceInInches);
@@ -170,6 +193,10 @@ public class Vision extends SubsystemBase {
     SmartDashboard.putNumber("Rounded Distance", roundedDistance);
   }
 
+  /* Based on distance, program will set the shooter motor to a certain speed. 
+  *  Also sets a variable to the same RPM the shooter motor will travel for use
+  *  in the AutomaticOpenGate
+  */
   public void AutomaticShooter()
   { 
     if (roundedDistance >= 15) //This if statement checks for the distance (see find distance) and picks RPM based on this
@@ -195,6 +222,8 @@ public class Vision extends SubsystemBase {
     m_shooterMotor.set(0);
   }
   
+  //Checks for a velocity provided by automaticshooter to check if gate can be opened
+  // (AND OPENS GATE)
   public void AutomaticOpenGate()
   {
     if (m_shooterEncoder.getVelocity() >= adjustableShooterRPM)
@@ -203,6 +232,7 @@ public class Vision extends SubsystemBase {
       m_shooterServo.setRaw(1000);
     }
   }
+
   public void AutomaticCloseGate()
   {
     m_shooterServo.setRaw(1300);
