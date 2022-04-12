3a3fb4570184d2d5126b7258ede5df053f4fcb14
Gerald10101 <gerald.schwarz@pridetronics.com>
2022-04-12 15:57:06 -0700

    aligh and aut intake partial coment

--- src/main/java/frc/robot/commands/AlignTarget.java
+++ src/main/java/frc/robot/commands/AlignTarget.java
@@ -1,7 +1,10 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+/*
+ * When called upon, executes seek target and Target adjust from vision subsystem consecutively and does not end because isfinished returns false
+ *
+ */
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -12,13 +15,14 @@ public class AlignTarget extends CommandBase {
 
   public AlignTarget(Vision vision) {
     m_vision = vision;
-    
+
     addRequirements(m_vision);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -29,12 +33,14 @@ public class AlignTarget extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false; //I'd like to add a command that checks when shooter run is finished or when it's toggled off. 
-    //That implies that some end command needs to exist. This one might be messy. 
+    return false; // I'd like to add a command that checks when shooter run is finished or when
+                  // it's toggled off.
+    // That implies that some end command needs to exist. This one might be messy.
   }
 }
