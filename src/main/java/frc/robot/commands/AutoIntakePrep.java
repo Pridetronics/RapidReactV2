9d1a886a579e225bf2d318e93b6477e735177aad
Katerina Deeter <46850117+KaterinaDeeter@users.noreply.github.com>
2022-04-12 20:25:05 -0700

    Adjusted autointakeprep (while drinking a horrible herbal tea that is reminiscent of stale watermelon Extra gum)

--- src/main/java/frc/robot/commands/AutoIntakePrep.java
+++ src/main/java/frc/robot/commands/AutoIntakePrep.java
@@ -2,8 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 /**
- * Executed first in autonomous period. 
- * 
+ * Executed second in autonomous period. This is the part of the 
+ * sequence that will allow the robot to drive out to the ball.
+ * Called intake prep, because it gets you to the position right before
+ * intake will be run. 
  */
 package frc.robot.commands;
 
@@ -27,12 +29,14 @@ public class AutoIntakePrep extends CommandBase {
   @Override
   public void initialize() {
     m_drive.zeroEncoders();
+    //These console messages are really helpful for ensuring functions are working. 
     System.out.println("AutoIntakePrep Started");
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //Checks if the encoders have reached a position (distance), and if not, runs the motors at 40%
     if (Math.abs(Drive.m_frontLeftEncoder.getPosition()) < intakeTarget) {
       m_drive.autoDriveIntakePrep();
     }
@@ -48,6 +52,7 @@ public class AutoIntakePrep extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    //Checks the position of the encoders, if at the proper position, the command should end
     if (Math.abs(Drive.m_frontLeftEncoder.getPosition()) > intakeTarget) {
       return true;
     } else {
