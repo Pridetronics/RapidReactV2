3242825589bbc7b4e91ed65915a7470ab12076e5
Gerald10101 <gerald.schwarz@pridetronics.com>
2022-09-16 15:28:41 -0700

    working robot code w/ second shooter motor and not working vision...?

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -24,82 +24,88 @@ import com.revrobotics.CANSparkMax.ControlType;
 import edu.wpi.first.wpilibj.Servo;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Shooter extends SubsystemBase {
-  //Hardware is created so it can be used within this subsytem. 
+  // Hardware is created so it can be used within this subsytem.
   private CANSparkMax m_shooterMotor;
   private RelativeEncoder m_shooterEncoder;
   private SparkMaxPIDController m_shooterPID;
   private Servo m_shooterServo;
- 
+
   public Shooter() {
-    //Motor, Encoder, PID, and Servo referenced from RobotContainer for use in subsytem.
-    m_shooterMotor = RobotContainer.shooterMotor;
+    // Motor, Encoder, PID, and Servo referenced from RobotContainer for use in
+    // subsytem.
+    m_shooterMotor = RobotContainer.shooterLeftMotor;
     m_shooterEncoder = RobotContainer.shooterEncoder;
     m_shooterPID = RobotContainer.shooterMotorPID;
     m_shooterServo = RobotContainer.shooterServo;
-    zeroEncoders(); //Resets the encoders (for accuracy-- better explained in Climb)
+    zeroEncoders(); // Resets the encoders (for accuracy-- better explained in Climb)
   }
 
   @Override
-  public void periodic() 
-  {}
+  public void periodic() {
+  }
 
-  //zeroEncoders: Resets Encoders-- Calls when subsytem is created. 
-  public void zeroEncoders() 
-  {
+  // zeroEncoders: Resets Encoders-- Calls when subsytem is created.
+  public void zeroEncoders() {
     m_shooterEncoder.setPosition(0);
   }
 
-  //LowSpeedShooter: Sets shooter motor to 2500 RPM. Also displays this on SmartDashboard
-  public void LowSpeedShooter() 
-  {
-    /* 
-    *  The line below sets the speeed for the motor using a PID loop. The first value is the RPM desired (human input),
-    *  and the second number is the ControlType (in this case velocity), which tells the encoder (Position is the other 
-    *  type-- see climb)
-    */
+  // LowSpeedShooter: Sets shooter motor to 2500 RPM. Also displays this on
+  // SmartDashboard
+  public void LowSpeedShooter() {
+    /*
+     * The line below sets the speeed for the motor using a PID loop. The first
+     * value is the RPM desired (human input),
+     * and the second number is the ControlType (in this case velocity), which tells
+     * the encoder (Position is the other
+     * type-- see climb)
+     */
     m_shooterPID.setReference(Constants.lowShooterSpeed, ControlType.kVelocity);
     SmartDashboard.putNumber("Shooter RPM", m_shooterEncoder.getVelocity());
   }
 
-  //HightSpeedShooter: Sets shooter motor to 5000 RPM and displays on SmartDashboard
-  public void HighSpeedShooter() 
-  { 
+  // HightSpeedShooter: Sets shooter motor to 5000 RPM and displays on
+  // SmartDashboard
+  public void HighSpeedShooter() {
     m_shooterPID.setReference(Constants.highShooterSpeed, ControlType.kVelocity);
     SmartDashboard.putNumber("Shooter RPM", m_shooterEncoder.getVelocity());
   }
 
-  //ShooterStop: Stops Shooter Motor
-  public void ShooterStop() 
-  { 
-    m_shooterMotor.set(0); 
-    //Set measures in voltage-- power outputted to the motor. In this case, 0. (Range = -1 to 1) 
+  // ShooterStop: Stops Shooter Motor
+  public void ShooterStop() {
+    m_shooterMotor.set(0);
+    // Set measures in voltage-- power outputted to the motor. In this case, 0.
+    // (Range = -1 to 1)
   }
 
-  /* 
-  *  OpenGateLow checks for a certain velocity (2500 in this case) and if the conditions are met, 
-  *  moves the servo gate to a position in which the ball can move through. Both open gate commands
-  *  ensure the motor is able to reach the proper speed so the ball is launched properly.
-  */
-  public void OpenGateLow() 
-  {
-    m_shooterServo.setRaw(Constants.shooterServoOpenPosition); //This is measured in ms-- see Pridetronics documentation for more information
-    
+  /*
+   * OpenGateLow checks for a certain velocity (2500 in this case) and if the
+   * conditions are met,
+   * moves the servo gate to a position in which the ball can move through. Both
+   * open gate commands
+   * ensure the motor is able to reach the proper speed so the ball is launched
+   * properly.
+   */
+  public void OpenGateLow() {
+    m_shooterServo.setRaw(Constants.shooterServoOpenPosition); // This is measured in ms-- see Pridetronics
+                                                               // documentation for more information
+
   }
 
-  /* 
-  *  OpenGateHigh checks for a certain velocity (5000 in this case) and if the conditions are met, 
-  *  moves the servo gate to a position in which the ball can move through.
-  */
-  public void OpenGateHigh()
-  {
-    new WaitCommand(7); //Test Code. Remove this in certain places
+  /*
+   * OpenGateHigh checks for a certain velocity (5000 in this case) and if the
+   * conditions are met,
+   * moves the servo gate to a position in which the ball can move through.
+   */
+  public void OpenGateHigh() {
+    new WaitCommand(7); // Test Code. Remove this in certain places
     m_shooterServo.setRaw(Constants.shooterServoOpenPosition);
   }
 
-  //CloseGate: Moves servo to a position in which the ball is unable to be released.
-  public void CloseGate()
-  {
+  // CloseGate: Moves servo to a position in which the ball is unable to be
+  // released.
+  public void CloseGate() {
     m_shooterServo.setRaw(Constants.shooterServoClosedPosition);
   }
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -27,7 +27,7 @@ import com.revrobotics.CANSparkMax.ControlType;
 import edu.wpi.first.wpilibj.Servo;
 
 public class Vision extends SubsystemBase {
-  //Hardware Relevant
+  // Hardware Relevant
   private CANSparkMax m_frontLeftMotor;
   private CANSparkMax m_rearLeftMotor;
   private CANSparkMax m_frontRightMotor;
@@ -43,7 +43,8 @@ public class Vision extends SubsystemBase {
   private SparkMaxPIDController m_shooterPIDController;
   private Servo m_shooterServo;
 
-  //The following is all involved in Limelight values. Explained further within functions
+  // The following is all involved in Limelight values. Explained further within
+  // functions
   private double ty;
   private double tx;
   private double tv;
@@ -72,7 +73,7 @@ public class Vision extends SubsystemBase {
     m_rearRightMotor = RobotContainer.rearRightMotor;
     m_rearRightPIDController = m_rearRightMotor.getPIDController();
 
-    m_shooterMotor = RobotContainer.shooterMotor;
+    m_shooterMotor = RobotContainer.shooterLeftMotor;
     m_shooterEncoder = RobotContainer.shooterEncoder;
     m_shooterPIDController = m_shooterMotor.getPIDController();
     m_shooterServo = RobotContainer.shooterServo;
@@ -80,66 +81,69 @@ public class Vision extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // While there are comments, this is a tricky topic-- check out the documentation if you need help
-    NetworkTableInstance inst = NetworkTableInstance.getDefault(); //get a reference to the subtable called "datatable"
+    // While there are comments, this is a tricky topic-- check out the
+    // documentation if you need help
+    NetworkTableInstance inst = NetworkTableInstance.getDefault(); // get a reference to the subtable called "datatable"
     NetworkTable table = inst.getTable("limelight");
     inst.startClientTeam(3853); // Make sure you set this to your team number
     inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
-    NetworkTableEntry yEntry = table.getEntry("ty"); 
+    NetworkTableEntry yEntry = table.getEntry("ty");
     NetworkTableEntry xEntry = table.getEntry("tx");
     NetworkTableEntry aEntry = table.getEntry("ta");
     NetworkTableEntry vEntry = table.getEntry("tv");
 
     ta = aEntry.getDouble(0.0); // Target Area (0% of image to 100% of image)
     ty = yEntry.getDouble(0.0); // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
-    tx = xEntry.getDouble(0.0); //Horizontal Offset From Crosshair to Target (-27.5 degrees to 27.5 degrees)
+    tx = xEntry.getDouble(0.0); // Horizontal Offset From Crosshair to Target (-27.5 degrees to 27.5 degrees)
     tv = vEntry.getDouble(0.0); // Whether the limelight has any valid targets (0 or 1)
 
-    SmartDashboard.putNumber("Limelight Area", ta); //Displays base limelight values to Shuffleboard
+    SmartDashboard.putNumber("Limelight Area", ta); // Displays base limelight values to Shuffleboard
     SmartDashboard.putNumber("Limelight X", tx);
     SmartDashboard.putNumber("Limelight Y", ty);
     SmartDashboard.putNumber("Limelight V", tv);
     // This method will be called once per scheduler run
   }
 
-  //lightsOut: Turns out LEDs, was created to be called in Robot Init (so the light wouldn't be on until needed)
-  public void lightsOut()
-  {
+  // lightsOut: Turns out LEDs, was created to be called in Robot Init (so the
+  // light wouldn't be on until needed)
+  public void lightsOut() {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
   }
 
   /*
-  * Processing Mode and Camera mode are for toggling between vision modes. 
-  * Processing mode prepares it for use with shooter/drive. CameraMode is solely for user vision
-  */
-  public void processingMode() 
-  {
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0); //Sets camMode to processing
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(2); //Tells LEDs to turn on (also there is an alternate 0, follows pipeline settings)
+   * Processing Mode and Camera mode are for toggling between vision modes.
+   * Processing mode prepares it for use with shooter/drive. CameraMode is solely
+   * for user vision
+   */
+  public void processingMode() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0); // Sets camMode to
+                                                                                              // processing
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(2); // Tells LEDs to turn on
+                                                                                              // (also there is an
+                                                                                              // alternate 0, follows
+                                                                                              // pipeline settings)
   }
 
-  public void cameraMode()
-  {
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1); //Sets camMode to camera vision  
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1); //Forces LEDs off. 
+  public void cameraMode() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1); // Sets camMode to camera
+                                                                                              // vision
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1); // Forces LEDs off.
   }
 
-  /* 
-  *  Seek Target: Checks if the Limelight has a target, if it does not, it will utilize PID control to
-  *  adjust until it finds a target. PID control is for movement speed. This is meant to keep it from
-  *  moving too fast (keeping it at 100 RPM). Then sets to 0 once target is found. 
-  */
-  public void SeekTarget() 
-  {
-    if (tv == 0) 
-    {
+  /*
+   * Seek Target: Checks if the Limelight has a target, if it does not, it will
+   * utilize PID control to
+   * adjust until it finds a target. PID control is for movement speed. This is
+   * meant to keep it from
+   * moving too fast (keeping it at 100 RPM). Then sets to 0 once target is found.
+   */
+  public void SeekTarget() {
+    if (tv == 0) {
       m_frontLeftPIDController.setReference(100, ControlType.kVelocity);
       m_rearRightPIDController.setReference(100, ControlType.kVelocity);
       m_frontRightPIDController.setReference(-100, ControlType.kVelocity);
       m_rearLeftPIDController.setReference(-100, ControlType.kVelocity);
-    } 
-    else if (tv == 1) 
-    {
+    } else if (tv == 1) {
       m_frontLeftPIDController.setReference(0, ControlType.kVelocity);
       m_frontRightPIDController.setReference(0, ControlType.kVelocity);
       m_rearLeftPIDController.setReference(0, ControlType.kVelocity);
@@ -147,11 +151,13 @@ public class Vision extends SubsystemBase {
     }
   }
 
-  /* 
-  *  TargetAdjust: Meant to center the robot on the horizontal axis, uses the drive train to turn slightly
-  *  based on where it falls within the angles of vision. If the target is too far right, it will turn left.
-  *  Too far left, it will turn right. If it is centered, do nothing. 
-  */
+  /*
+   * TargetAdjust: Meant to center the robot on the horizontal axis, uses the
+   * drive train to turn slightly
+   * based on where it falls within the angles of vision. If the target is too far
+   * right, it will turn left.
+   * Too far left, it will turn right. If it is centered, do nothing.
+   */
   public void TargetAdjust() {
     if (tx > 3 && tx < 28) // turning left
     {
@@ -159,16 +165,13 @@ public class Vision extends SubsystemBase {
       m_rearLeftPIDController.setReference(50, ControlType.kVelocity);
       m_frontLeftPIDController.setReference(-50, ControlType.kVelocity);
       m_rearRightPIDController.setReference(-50, ControlType.kVelocity);
-    } 
-    else if (tx < -3 && tx > -28) // turning right
+    } else if (tx < -3 && tx > -28) // turning right
     {
       m_frontLeftPIDController.setReference(50, ControlType.kVelocity);
       m_rearRightPIDController.setReference(50, ControlType.kVelocity);
       m_frontRightPIDController.setReference(-50, ControlType.kVelocity);
       m_rearLeftPIDController.setReference(-50, ControlType.kVelocity);
-    } 
-    else 
-    {
+    } else {
       m_frontLeftPIDController.setReference(0, ControlType.kVelocity);
       m_frontRightPIDController.setReference(0, ControlType.kVelocity);
       m_rearLeftPIDController.setReference(0, ControlType.kVelocity);
@@ -176,92 +179,88 @@ public class Vision extends SubsystemBase {
     }
   }
 
-  //findDistance: Calculates distance and converts it into rounded distance for use within the automatic shooter code
-  public void findDistance()
-  {
-    //Equation for distance is d = (h1 - h2)/(tan(a1 + a2)) //Defaults to meters/radian
-    heightTotal = 104 - 25.75; //Input inches
-    angleTotal = 0.628 + ty; //Input radians
+  // findDistance: Calculates distance and converts it into rounded distance for
+  // use within the automatic shooter code
+  public void findDistance() {
+    // Equation for distance is d = (h1 - h2)/(tan(a1 + a2)) //Defaults to
+    // meters/radian
+    heightTotal = 104 - 25.75; // Input inches
+    angleTotal = 0.628 + ty; // Input radians
     angleTan = Math.tan(angleTotal);
 
-    initialDistance = Math.abs(heightTotal/angleTan); //Returns negative, so there's a negative on the next line
-    distanceInInches = initialDistance * 13.6; //Converts distance into inches. 
-    distanceInFeet = distanceInInches/12; //Converts distance in inches to feet
-    roundedDistance = Math.round(distanceInFeet); //Rounds distance in feet
-    
-    SmartDashboard.putNumber("Initial Distance", initialDistance); //Puts all these values on the smartdashboard when run. This is solely for testing purposes
+    initialDistance = Math.abs(heightTotal / angleTan); // Returns negative, so there's a negative on the next line
+    distanceInInches = initialDistance * 13.6; // Converts distance into inches.
+    distanceInFeet = distanceInInches / 12; // Converts distance in inches to feet
+    roundedDistance = Math.round(distanceInFeet); // Rounds distance in feet
+
+    SmartDashboard.putNumber("Initial Distance", initialDistance); // Puts all these values on the smartdashboard when
+                                                                   // run. This is solely for testing purposes
     SmartDashboard.putNumber("Distance in Inches", distanceInInches);
     SmartDashboard.putNumber("Distance in Feet", distanceInFeet);
     SmartDashboard.putNumber("Rounded Distance", roundedDistance);
   }
 
-  /* 
-  *  AutomaticShooter: Based on distance, program will set the shooter motor to a 
-  *  certain speed.  Also sets a variable to the same RPM the shooter motor will 
-  *  travel for use in the AutomaticOpenGate
-  */
-  public void AutomaticShooter()
-  { 
-    if (roundedDistance > 13) //This if statement checks for the distance (see find distance) and picks RPM based on this
+  /*
+   * AutomaticShooter: Based on distance, program will set the shooter motor to a
+   * certain speed. Also sets a variable to the same RPM the shooter motor will
+   * travel for use in the AutomaticOpenGate
+   */
+  public void AutomaticShooter() {
+    if (roundedDistance > 13) // This if statement checks for the distance (see find distance) and picks RPM
+                              // based on this
     {
-      //This line sets the PID controller to listed RPM (first number), add control type so controller knows which value is being impacted
+      // This line sets the PID controller to listed RPM (first number), add control
+      // type so controller knows which value is being impacted
       m_shooterPIDController.setReference(Constants.shooterRPMHigh, ControlType.kVelocity);
       adjustableShooterRPM = Constants.shooterRPMHigh;
-    }
-    else if (roundedDistance <= 13 && roundedDistance >= 11)
-    {
+    } else if (roundedDistance <= 13 && roundedDistance >= 11) {
       m_shooterPIDController.setReference(Constants.shooterRPMMedium, ControlType.kVelocity);
       adjustableShooterRPM = Constants.shooterRPMMedium;
-    }
-    else if (roundedDistance <= 10)
-    {
+    } else if (roundedDistance <= 10) {
       m_shooterPIDController.setReference(Constants.shooterRPMLow, ControlType.kVelocity);
       adjustableShooterRPM = Constants.shooterRPMLow;
     }
   }
 
-  //ShooterMotorStop: Stops shooter motor
-  public void ShooterMotorStop()
-  {
+  // ShooterMotorStop: Stops shooter motor
+  public void ShooterMotorStop() {
     m_shooterMotor.set(0);
   }
 
-  public void DriveMotorsStop()
-  {
+  public void DriveMotorsStop() {
     m_frontLeftMotor.set(0);
     m_frontRightMotor.set(0);
     m_rearLeftMotor.set(0);
     m_rearRightMotor.set(0);
   }
-  
-  //AutomaticOpenGate: Checks for a velocity provided by automaticshooter to check if open gate conditions are met (then opens gate)
-  public void AutomaticOpenGate()
-  {
-      m_shooterServo.setRaw(Constants.shooterServoOpenPosition);
+
+  // AutomaticOpenGate: Checks for a velocity provided by automaticshooter to
+  // check if open gate conditions are met (then opens gate)
+  public void AutomaticOpenGate() {
+    m_shooterServo.setRaw(Constants.shooterServoOpenPosition);
   }
 
-  //AutomaticCloseGate: Puts servo in position where balls cannot get through
-  public void AutomaticCloseGate()
-  {
+  // AutomaticCloseGate: Puts servo in position where balls cannot get through
+  public void AutomaticCloseGate() {
     m_shooterServo.setRaw(Constants.shooterServoClosedPosition);
   }
 
 }
 
 /*
-———————————No Limelight?———————————
-⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
-⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
-⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
-⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
-⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
-⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-—————————————————————————————
-*/
+ * ———————————No Limelight?———————————
+ * ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
+ * ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
+ * ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
+ * ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
+ * ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * —————————————————————————————
+ */
